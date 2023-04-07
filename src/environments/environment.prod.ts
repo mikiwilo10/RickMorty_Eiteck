4bf425e579215df7dc709257e2b218d8d74afd9e
mikiwilo10 <mikiwilo10@gmail.com>
2023-04-07 13:06:09 -0500

    Build Production

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  "characters": "https://rickandmortyapi.com/api/character",
-  "locations": "https://rickandmortyapi.com/api/location",
-  "episodes": "https://rickandmortyapi.com/api/episode"
+  characters: "https://rickandmortyapi.com/api/character",
+  locations: "https://rickandmortyapi.com/api/location",
+  episodes: "https://rickandmortyapi.com/api/episode"
 };
